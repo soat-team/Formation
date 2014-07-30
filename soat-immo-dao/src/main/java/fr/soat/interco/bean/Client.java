e5048ec2c2448859d50763da4f902b52f3f1e919
Clarence Charles <clarence.charles@soat.fr>
2014-07-30 17:13:55 +0200

    Creation d'un fichier de migration

--- soat-immo-dao/src/main/java/fr/soat/interco/bean/Client.java
+++ soat-immo-dao/src/main/java/fr/soat/interco/bean/Client.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 /**
  * Class qui représente un client
@@ -13,19 +14,21 @@ public class Client {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Integer idclient;
 
     private String nom;
     private String prenom;
+    private Date date_de_naissance;
     private Integer salaire;
     private boolean periodEssai;
+    private String criteres_recherche;
 
-    public Integer getId() {
-        return id;
+    public Integer getIdclient() {
+        return idclient;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setIdclient(Integer idclient) {
+        this.idclient = idclient;
     }
 
     public String getNom() {
@@ -58,5 +61,21 @@ public class Client {
 
     public void setPeriodEssai(boolean periodEssai) {
         this.periodEssai = periodEssai;
+    }
+
+    public Date getDate_de_naissance() {
+        return date_de_naissance;
+    }
+
+    public void setDate_de_naissance(Date date_de_naissance) {
+        this.date_de_naissance = date_de_naissance;
+    }
+
+    public String getCriteres_recherche() {
+        return criteres_recherche;
+    }
+
+    public void setCriteres_recherche(String criteres_recherche) {
+        this.criteres_recherche = criteres_recherche;
     }
 }
