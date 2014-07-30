c45ccb0cc80825d9f73c487ad1be5a6e3c7d9bb5
feriell <lamrous.feriel@gmail.com>
2014-07-30 16:08:47 +0200

    Fichier sql
    Ajout de l'agent/visite/client

--- soat-immo-dao/src/main/resources/soat_immo.sql
+++ soat-immo-dao/src/main/resources/soat_immo.sql
@@ -1,4 +1,4 @@
-CREATE SCHEMA IF NOT EXISTS `SoatImmo` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci 
+CREATE SCHEMA IF NOT EXISTS `SoatImmo` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
 
 
 CREATE TABLE IF NOT EXISTS `SoatImmo`.`bien_immobilier` (
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS `SoatImmo`.`bien_immobilier` (
   `publication_seLoger` TINYINT(1) NULL,
   `url_photo` VARCHAR(45) NULL,
   PRIMARY KEY (`idbien_immobilier`))
-  ENGINE = InnoDB
+  ENGINE = InnoDB;
   
   
   CREATE TABLE IF NOT EXISTS `SoatImmo`.`client` (
@@ -31,4 +31,33 @@ CREATE TABLE IF NOT EXISTS `SoatImmo`.`bien_immobilier` (
   `periode_essai` TINYINT(1) NULL,
   `criteres_recherche` VARCHAR(100) NULL,
   PRIMARY KEY (`idclient`))
-ENGINE = InnoDB
+ENGINE = InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS `SoatImmo`.`visite` (
+  `idvisite` INT NOT NULL,
+  `heure_debut` DATETIME NOT NULL,
+  `heure_fin` DATETIME NOT NULL,
+  `id_agent` INT NOT NULL,
+  `id_bien_immo` INT NOT NULL,
+  PRIMARY KEY (`idvisite`),
+  INDEX `id_agent_idx` (`id_agent` ASC),
+  INDEX `id_bien_immobilier_idx` (`id_bien_immo` ASC),
+  CONSTRAINT `id_agent`
+    FOREIGN KEY (`id_agent`)
+    REFERENCES `SoatImmo`.`agent` (`idagent`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `id_bien_immobilier`
+    FOREIGN KEY (`id_bien_immo`)
+    REFERENCES `SoatImmo`.`bien_immobilier` (`idbien_immobilier`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `SoatImmo`.`agent` (
+  `idagent` INT NOT NULL,
+  `nom` VARCHAR(45) NOT NULL,
+  `prenom` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idagent`))
+ENGINE = InnoDB;
