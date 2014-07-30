b1544a4bd67cd87556fe5be46b04479ba8c2433a
feriell <lamrous.feriel@gmail.com>
2014-07-30 15:34:04 +0200

    Fichier sql
    Ajout de la table Client

--- soat-immo-dao/src/main/resources/soat_immo.sql
+++ soat-immo-dao/src/main/resources/soat_immo.sql
@@ -1,3 +1,6 @@
+CREATE SCHEMA IF NOT EXISTS `SoatImmo` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci 
+
+
 CREATE TABLE IF NOT EXISTS `SoatImmo`.`bien_immobilier` (
   `idbien_immobilier` INT NOT NULL AUTO_INCREMENT,
   `type` VARCHAR(45) NULL,
@@ -16,4 +19,16 @@ CREATE TABLE IF NOT EXISTS `SoatImmo`.`bien_immobilier` (
   `publication_seLoger` TINYINT(1) NULL,
   `url_photo` VARCHAR(45) NULL,
   PRIMARY KEY (`idbien_immobilier`))
+  ENGINE = InnoDB
+  
+  
+  CREATE TABLE IF NOT EXISTS `SoatImmo`.`client` (
+  `idclient` INT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(45) NULL,
+  `prenom` VARCHAR(45) NULL,
+  `date_de_naissance` DATETIME NULL,
+  `salaire` VARCHAR(45) NULL,
+  `periode_essai` TINYINT(1) NULL,
+  `criteres_recherche` VARCHAR(100) NULL,
+  PRIMARY KEY (`idclient`))
 ENGINE = InnoDB
