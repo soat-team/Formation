a8a38aa481e19538c97d5ef6d86fd5e374c488f3
Clarence Charles <clarence.charles@soat.fr>
2014-07-30 10:35:25 +0200

    Initial commit

---
+++ soat-immo-dao/src/main/java/fr/soat/interco/App.java
@@ -0,0 +1,13 @@
+package fr.soat.interco;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
