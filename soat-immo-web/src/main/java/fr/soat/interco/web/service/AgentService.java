1817476115d5ddbcd2eedf1f7bac81896a196d90
HamzaLAZRAK <hamza.lazrak@soat.fr>
2014-07-31 12:00:56 +0200

    implémentation AgentService & AgentServiceImpl

---
+++ soat-immo-web/src/main/java/fr/soat/interco/web/service/AgentService.java
@@ -0,0 +1,51 @@
+package fr.soat.interco.web.service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.soat.interco.bean.Agent;
+import fr.soat.interco.bean.Client;
+
+
+/**
+ * @author Formation
+ *
+ */
+
+@Transactional
+public interface AgentService {
+	
+	
+    /**
+     * @param nom
+     * @param prenom
+     * @return
+     */
+    public Agent findAgentByNomAndPrenom(String nom, String prenom);
+
+
+    /**
+     * @return
+     */
+    public Iterable<Agent> findAgents();
+
+    
+    /**
+     * @param agent
+     * @return
+     */
+    public Agent saveAgent(Agent agent);
+
+   
+    /**
+     * @param id
+     * @return
+     */
+    public Agent findAgentById(Integer id);
+
+   
+    /**
+     * @param agent
+     */
+    public void deleteAgent(Agent agent);
+
+}
---
+++ soat-immo-web/src/main/java/fr/soat/interco/web/service/AgentServiceImpl.java
@@ -0,0 +1,40 @@
+package fr.soat.interco.web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fr.soat.interco.bean.Agent;
+import fr.soat.interco.dao.AgentDao;
+
+@Service
+public class AgentServiceImpl implements AgentService {
+
+	@Autowired
+	private AgentDao agentDao;
+	
+	@Override
+	public Agent findAgentByNomAndPrenom(String nom, String prenom) {
+		return findAgentByNomAndPrenom(nom, prenom);
+	}
+
+	@Override
+	public Iterable<Agent> findAgents() {
+		return agentDao.findAll();
+	}
+
+	@Override
+	public Agent saveAgent(Agent agent) {
+		return agentDao.save(agent);
+	}
+
+	@Override
+	public Agent findAgentById(Integer id) {
+		return agentDao.findOne(id);
+	}
+
+	@Override
+	public void deleteAgent(Agent agent) {
+		agentDao.delete(agent);
+	}
+
+}
